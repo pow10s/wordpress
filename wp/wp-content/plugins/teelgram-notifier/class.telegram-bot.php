92d8e4413c98464dcdeb513a6264727136e3a8ec
Dima Stos <Dima Stos>
2017-11-27 01:45:56 +0200

    ci-edit-folder name

--- wp/wp-content/plugins/teelgram-notifier/class.telegram-bot.php
+++ wp/wp-content/plugins/teelgram-notifier/class.telegram-bot.php
@@ -317,7 +317,7 @@ class Telegram_Bot
     public function setWebhook()
     {
         if (isset($_REQUEST['page']) && $_REQUEST['page'] == 'telegram-settings') {
-            $pluginUrl = plugins_url('telegram-notifier/bot.php');
+            $pluginUrl = plugins_url('teelgram-notifier/bot.php');
             print_r($pluginUrl);
             try {
                 $this->api->setWebhook($pluginUrl);
